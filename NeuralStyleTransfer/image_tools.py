62dd3467277e3210f61a1f6b442c5cf0fbdecfdc
Prabesh Raj Joshi <prabeshrajjoshi@gmail.com>
2020-11-04 18:51:34 +0100

    shift image path initialization to module

--- NeuralStyleTransfer/image_tools.py
+++ NeuralStyleTransfer/image_tools.py
@@ -1,3 +1,12 @@
 # pjoshi, 11.2020
 # python module containing tools for neural style transfer 
 
+def get_drive_images(COLABPATH):
+    '''
+    function to retrieve path to content, style, and generated images
+    '''
+    base_image_path = COLABPATH + "/images/content.png"
+    style_reference_image_path = COLABPATH + "/images/style.png"
+    result_prefix = COLABPATH + "/output_images/generated"
+
+    return (base_image_path, style_reference_image_path, result_prefix)
